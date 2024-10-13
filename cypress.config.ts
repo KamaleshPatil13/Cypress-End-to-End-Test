cb455ef75ed74d2e8052620050bf3973561d63d5
13kamaleshpatil <13kamaleshpatil@gmail.com>
2024-10-13 16:21:12 +0530

    Update the cypress package version.

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     experimentalModifyObstructiveThirdPartyCode: true,
-    baseUrl: 'https://example.cypress.io ',
+    baseUrl: 'http://localhost:5173',
   },
 });
